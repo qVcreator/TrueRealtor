22f0af767c040aa5a51fdbb023dbb5d05d6cf513
Timur <2001rich04@gmail.com>
2022-10-04 17:37:50 +0300

    Add AdminsService

---
+++ TrueRealtor.Business/Services/AdminsService.cs
@@ -0,0 +1,23 @@
+﻿using TrueRealtor.Business.Interfaces;
+using TrueRealtor.Data.Entities;
+using TrueRealtor.Data.Interfaces;
+
+namespace TrueRealtor.Business.Services;
+
+public class AdminsService : IAdminsService
+{
+    private readonly IAdminsRepository _adminsRepository;
+
+    public AdminsService(IAdminsRepository adminsRepository)
+    {
+        _adminsRepository = adminsRepository;
+    }
+
+    public async Task<int> AddApartment(Apartment apartment)
+    {
+        //Add checking on existing similar item
+        var result = await _adminsRepository.AddApartment(apartment);
+
+        return result;
+    }
+}
