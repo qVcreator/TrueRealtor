aa155945bc9d30640cadd5a30833920e626aee75
Timur <2001rich04@gmail.com>
2022-10-04 18:02:28 +0300

    Add TrueRealtorContext

---
+++ TrueRealtor.Data/TrueRealtorContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrueRealtor.Data.Constants;
+using TrueRealtor.Data.Entities;
+
+namespace TrueRealtor.Data
+{
+    public class TrueRealtorContext : DbContext
+    {
+        public DbSet<Apartment> Apartments { get; set; }
+
+        public TrueRealtorContext(DbContextOptions<TrueRealtorContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Apartment>(entity =>
+            {
+                entity.ToTable(nameof(Apartment));
+
+                entity.HasKey(a => a.Id);
+
+                entity.Property(a => a.Address).HasMaxLength(DbConstant.AddressMaxLenght);
+            });
+        }
+    }
+}
