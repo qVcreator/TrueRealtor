fe8d26476a211c97bd64ffa10796790dfc24fc42
Timur <2001rich04@gmail.com>
2022-10-04 16:38:49 +0300

    Add ApartmentEntity

---
+++ TrueRealtor.Data/Entities/Apartment.cs
@@ -0,0 +1,9 @@
+﻿namespace TrueRealtor.Data.Entities;
+
+public class Apartment
+{
+    public int Id { get; set; }
+    public decimal Price { get; set; }
+    public int CityId { get; set; }
+    public string Address { get; set; }
+}
