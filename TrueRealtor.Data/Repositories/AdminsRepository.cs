a1daec4ef6a7be3b14e92efc20520b7290949daa
Timur <2001rich04@gmail.com>
2022-10-04 18:06:12 +0300

    Add AdminsRepository

---
+++ TrueRealtor.Data/Repositories/AdminsRepository.cs
@@ -0,0 +1,22 @@
+﻿using TrueRealtor.Data.Entities;
+using TrueRealtor.Data.Interfaces;
+
+namespace TrueRealtor.Data.Repositories;
+
+public class AdminsRepository : IAdminsRepository
+{
+    private readonly TrueRealtorContext _context;
+
+    public AdminsRepository(TrueRealtorContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> AddApartment(Apartment apartment)
+    {
+        await _context.AddAsync(apartment);
+        await _context.SaveChangesAsync();
+
+        return apartment.Id;
+    }
+}
