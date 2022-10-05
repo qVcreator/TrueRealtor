1d9d26723e978542b6d7c36a8f708a5b485ef7e4
Timur <2001rich04@gmail.com>
2022-10-05 17:26:47 +0300

    Add AdminsLayers

---
+++ TrueRealtor.Business/Interfaces/IAdminsService.cs
@@ -0,0 +1,10 @@
+﻿using TrueRealtor.Data.Entities;
+
+namespace TrueRealtor.Business.Interfaces;
+
+public interface IAdminsService
+{
+    Task<int> AddAdmin(User user);
+    Task<int> AddManager(User user);
+    Task<User> GetUserByEmail(string email);
+}
---
+++ TrueRealtor.Data/Interfaces/IAdminsRepository.cs
@@ -0,0 +1,11 @@
+﻿using TrueRealtor.Data.Entities;
+
+namespace TrueRealtor.Data.Interfaces;
+
+public interface IAdminsRepository
+{
+    Task<int> AddAdmin(User user);
+    Task<int> AddManager(User user);
+    Task<User> GetUserByEmail(string email);
+
+} 
