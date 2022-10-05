af54aac0f1e124304886457ae4aea6032709184d
Timur <2001rich04@gmail.com>
2022-10-05 16:54:04 +0300

    Add AuthController

---
+++ TrueRealtor.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrueRealtor.API.Extensions;
+using TrueRealtor.API.Requests;
+using TrueRealtor.Business.Interfaces;
+
+namespace TrueRealtor.API.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<string>> Login(LoginRequest authParams)
+    {
+        var user = await _authService.GetUserByLogin(authParams.Email, authParams.Password);
+
+        if(user == null)
+            return Unauthorized();
+
+        var userId = this.GetUserId();
+
+        if (userId is not null && userId.Value != user.Id)
+            return Forbid();
+
+        return await _authService.GetToken(user);
+    }
+}
--- TrueRealtor.API/Extensions/ControllerExtension.cs
+++ TrueRealtor.API/Extensions/ControllerExtension.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace TrueRealtor.API.Extensions;
 
 public static class ControllerExtension
 {
-    public static string GetUri(this Controller c) =>
-        $"{c.Request?.Scheme}://{c.Request?.Host.Value}{c.Request?.Path.Value}";
+    public static string GetUri(this Controller controller) =>
+        $"{controller.Request?.Scheme}://{controller.Request?.Host.Value}{controller.Request?.Path.Value}";
+
+    public static int? GetUserId(this Controller controller)
+    {
+        if (controller.HttpContext.User.Identity is not ClaimsIdentity identity)
+            return null;
+
+        if (!int.TryParse(identity.FindFirst(ClaimTypes.NameIdentifier)?.Value, out var userId))
+            return null;
+
+        return userId;
+    }
 }
