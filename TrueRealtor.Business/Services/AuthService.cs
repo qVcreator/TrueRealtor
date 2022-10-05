bfb7ebd581a3087d166fe6dd1804c53195c77f29
Timur <2001rich04@gmail.com>
2022-10-05 17:26:54 +0300

    Add Auth

--- TrueRealtor.Business/Services/AuthService.cs
+++ TrueRealtor.Business/Services/AuthService.cs
@@ -1,17 +1,40 @@
-﻿using TrueRealtor.Business.Interfaces;
+﻿using TrueRealtor.Business.Infrastructure;
+using TrueRealtor.Business.Interfaces;
 using TrueRealtor.Data.Entities;
+using TrueRealtor.Data.Enums;
 
 namespace TrueRealtor.Business.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly IAdminsService _adminsService;
+
+    public AuthService(IAdminsService adminsService)
+    {
+        _adminsService = adminsService;
+    }
     public Task<string> GetToken(User user)
     {
         throw new NotImplementedException();
     }
 
-    public Task<User> GetUserByLogin(string email, string password)
+    public async Task<User> GetUserByLogin(string email, string password)
     {
-        throw new NotImplementedException();
+        var result = new User();
+
+        var user = await _adminsService.GetUserByEmail(email);
+
+        if (user == null)
+            throw new Exception();// custom later
+
+        if (user is not null && email == user.Email &&
+            Hash.ValidatePassword(password, user.Password))
+        {
+            result.Email = email;
+            result.Role = user.Role;
+            result.Id = user.Id;
+        }
+
+        return result;
     }
 }
